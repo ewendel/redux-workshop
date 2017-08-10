f0f037668b5964f9d182570e44c64d231fd70fc2
Ingar Almklov <ingara@gmail.com>
2017-08-10 08:56:39 +0200

    Simplify CurrentTweet

--- src/components/CurrentTweet.jsx
+++ src/components/CurrentTweet.jsx
@@ -5,7 +5,7 @@ import Tweet from './Tweet';
 export default function CurrentTweet(props) {
   return (
     <div className="current-tweet">
-      <Tweet { ...props } />
+      <Tweet tweet={ props.tweet } />
     </div>
   );
 }
